c7a5571a6a3c9504d1624bef48e8553a0c1da40b
Juan <juanan0309@gmail.com>
2021-10-19 14:07:10 -0500

    Add initial setup

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+import LoginScreen from "./screens/LoginScreen";
+import SearchScreen from "./screens/SearchScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+const icons = {
+  SearchStack: "tools",
+  Profile: "user",
+};
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const DetailStack = createNativeStackNavigator();
+
+function SearchStack({ navigation }) {
+  return (
+    <DetailStack.Navigator>
+      <DetailStack.Screen name="Services" component={SearchScreen} />
+    </DetailStack.Navigator>
+  );
+}
+
+function LandingScreen() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          const iconName = icons[route.name] || "tools";
+
+          return <FontAwesome5 name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        name="SearchStack"
+        options={{
+          title: "Services",
+          headerShown: false,
+        }}
+        component={SearchStack}
+      />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen
+          name="Login"
+          options={{ headerShown: false }}
+          component={LoginScreen}
+        /> */}
+
+        <Stack.Screen
+          name="Home"
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
